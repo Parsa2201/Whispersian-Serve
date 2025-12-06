15468d1c96b159547f8dab32605e4269821a078b
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-12-06 13:55:09 +0330

    feat: config manager

---
+++ src/config.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SAMPLE_RATE = os.environ.get("SAMPLE_RATE")
+BATCH_SIZE = os.environ.get("BATCH_SIZE")
+
+# DEVICE = "cpu"
+# if torch.cuda.is_available():
+#     DEVICE = "gpu"
+# elif torch.backends.mps.is_available():
+#     DEVICE = "mps"
+# DEVICE = "gpu" if torch.cuda.is_available() else "cpu"
+
+MODEL_TYPE = os.environ.get("MODEL_TYPE")
+MODEL_PATH = os.environ.get("MODEL_PATH")
+
+SAMPLE_AUDIO = os.environ.get("SAMPLE_AUDIO")
