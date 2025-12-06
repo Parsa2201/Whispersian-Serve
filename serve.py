085ecc7f29d66da4e847c6230d9bb41672a5ff89
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-12-06 13:55:24 +0330

    feat: fastAPI server backend

---
+++ serve.py
@@ -0,0 +1,21 @@
+import os
+from fastapi import FastAPI, File, UploadFile
+from src.transcribe.onnx_trans import OnnxTrans
+from src.config import *
+
+app = FastAPI()
+
+@app.post("/transcribe")
+async def transcribe_audio(file: UploadFile):
+    audio_bytes = await file.read()
+
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    audio_path = f"data/{file.filename}"
+    with open(audio_path, "wb") as f_out:
+        f_out.write(audio_bytes)
+
+    model = OnnxTrans(model_path=MODEL_PATH)
+    text = model.transcribe([audio_path])[0]
+
+    return {"text": text}
